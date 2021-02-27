2c8baac81391f8993a6db4917c3805387b9dbf14
KevinZhang <zhang_kun_lin@outlook.com>
2021-02-27 21:36:04 +0800

    add test.java

--- test.java
+++ test.java
@@ -1,1 +1,26 @@
-pu
+
+import java.util.*;
+
+
+
+class test {
+  public static void main(String[] args) {
+    Stack<Integer> s;
+    Deque<Integer> q;
+
+    s = new Stack<>();
+    q = new ArrayDeque<>();
+
+    q.addLast(Integer.valueOf(1));
+    q.addLast(Integer.valueOf(2));
+    q.addLast(Integer.valueOf(3));
+ 
+    System.out.println(q.removeFirst();
+    System.out.println(q.poll());
+
+    System.out.println(q.peek());
+    System.out.println(q.remove());
+  
+    System.out.println(q.peek());
+  }
+}
