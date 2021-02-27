2fa592d08a884eedc3650a6b3dbfe757a41ed501
KevinZhang <zhang_kun_lin@outlook.com>
2021-02-27 21:59:19 +0800

    test.java

--- test.java
+++ test.java
@@ -15,7 +15,7 @@ class test {
     q.addLast(Integer.valueOf(2));
     q.addLast(Integer.valueOf(3));
  
-    System.out.println(q.removeFirst();
+    System.out.println(q.removeFirst());
     System.out.println(q.poll());
 
     System.out.println(q.peek());
